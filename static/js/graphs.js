678ab44ff82191d2cd593ae48b76a3aff218d8ac
James Gardiner <jamesg87@me.com>
2016-09-14 15:00:43 +0100

    Remove mouseover events

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -83,27 +83,6 @@ function deselectLayer(layer) {
     });
 }
 
-function enterLayer() {
-    this.bringToFront();
-    this.setStyle({
-        weight: 3,
-        opacity: 1,
-        fillOpacity: 1,
-        color: '#666666'
-    });
-    info.update(this.feature.properties);
-}
-
-function leaveLayer() {
-    this.bringToBack();
-    this.setStyle({
-        weight: 1,
-        opacity: 0.5,
-        fillOpacity: 0.7,
-        color: 'white'
-    });
-}
-
 //function called when the selected area is changed
 function areaChanged() {
     if (selectedArea === undefined) {
@@ -127,182 +106,182 @@ function areaChanged() {
     });
 }
 
-    // Initialise global map and layer variables
-    var map = new L.map('map');
-    var topoLayer = new L.TopoJSON();
-    var colorScale = chroma
-        .scale(chroma.brewer.YlOrRd)
-        .domain([-2.1227372118474177, 2.397453389303414]);
+// Initialise global map and layer variables
+var map = new L.map('map');
+var topoLayer = new L.TopoJSON();
+var colorScale = chroma
+    .scale(chroma.brewer.YlOrRd)
+    .domain([-2.1227372118474177, 2.397453389303414]);
 
-    // Information control
-    var info = L.control();
+// Information control
+var info = L.control();
 
-    info.onAdd = function(map) {
-        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-        this.update();
-        return this._div;
-    };
+info.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
 
-    // method that we will use to update the control based on feature properties passed
-    info.update = function(props) {
-        this._div.innerHTML = '<h4>Local Authority Complexity</h4>' + (props ?
-            '<b>' + props.LAD13NM + '</b><br />' + props.complexity : 'Hover over a Local Authority Area');
-    };
+// method that we will use to update the control based on feature properties passed
+info.update = function(props) {
+    this._div.innerHTML = '<h4>Local Authority Complexity</h4>' + (props ?
+        '<b>' + props.LAD13NM + '</b><br />' + props.complexity : 'Hover over a Local Authority Area');
+};
 
-    info.addTo(map);
+info.addTo(map);
 
-    // Area Selection Control
-    //extend the L.Control class to create a custom drop down box
-    // initially with simple placeholder text
-    var AreaControl = L.Control.extend({
-        initialize: function(name, options) {
-            L.Util.setOptions(this, options);
-        },
-        //function to be called when the control is added to the map
-        onAdd: function(map) {
-            //create the control container with a css class name
-            var container = L.DomUtil.create('div', 'area-control');
-            //jquery method to retrieve JSON object
-            $.getJSON("data/LAD_2014_UK_NC.json",
-                function(data) {
-                    //create the htmlString that will form the
-                    //innerHTML of the forces dropdown
-                    var htmlString = '<select id=areas ' +
-                        'onchange="areaChanged()">';
-                    //create individual area <option> tags within the
-                    //<select> tag
-                    $.each(data, function(i, item) {
-                        areas[i] = item;
-                        htmlString = htmlString + '<option>' +
-                            areas[i].LAD14NM + '</option>';
-                    });
-
-                    //close the select tag
-                    htmlString += '</select>';
-
-                    //update the dropdown list innerHTML
-                    //with the list of forces
-                    container.innerHTML = htmlString;
-
-                    //allow a user to select a single option
-                    container.firstChild.onmousedown =
-                        container.firstChild.ondblclick =
-                        L.DomEvent.stopPropagation;
-
+// Area Selection Control
+//extend the L.Control class to create a custom drop down box
+// initially with simple placeholder text
+var AreaControl = L.Control.extend({
+    initialize: function(name, options) {
+        L.Util.setOptions(this, options);
+    },
+    //function to be called when the control is added to the map
+    onAdd: function(map) {
+        //create the control container with a css class name
+        var container = L.DomUtil.create('div', 'area-control');
+        //jquery method to retrieve JSON object
+        $.getJSON("data/LAD_2014_UK_NC.json",
+            function(data) {
+                //create the htmlString that will form the
+                //innerHTML of the forces dropdown
+                var htmlString = '<select id=areas ' +
+                    'onchange="areaChanged()">';
+                //create individual area <option> tags within the
+                //<select> tag
+                $.each(data, function(i, item) {
+                    areas[i] = item;
+                    htmlString = htmlString + '<option>' +
+                        areas[i].LAD14NM + '</option>';
                 });
-            return container;
+
+                //close the select tag
+                htmlString += '</select>';
+
+                //update the dropdown list innerHTML
+                //with the list of forces
+                container.innerHTML = htmlString;
+
+                //allow a user to select a single option
+                container.firstChild.onmousedown =
+                    container.firstChild.ondblclick =
+                    L.DomEvent.stopPropagation;
+
+            });
+        return container;
+    }
+});
+
+map.addControl(new AreaControl('areas', {
+    position: 'topright'
+}));
+
+
+/* Dynamic resizing */
+$(window).on("resize", function() {
+    var h = $(window).height();
+    $("#map").height(h - (h * 0.2));
+    map.invalidateSize();
+}).trigger("resize");
+
+loadData();
+
+/*--------------------------------------------------------------*/
+/* Charts */
+
+var chart;
+
+function currentSelectedIndustry() {
+    return $("#indList").val();
+}
+
+function parseData(n_businesses_data, prob, prod_complexity, growth_data) {
+    var marker_data = [];
+    var x = [];
+    var y = [];
+    var color = [];
+    var text = [];
+    var size = [];
+    for (var key in n_businesses_data.S12000033) {
+        if (key !== "growth" && key !== "LAD14NM") {
+            var n_businesses = n_businesses_data.S12000033[key];
+            var probability = prob.S12000033[key];
+            var complexity_value = prod_complexity[key].score;
+            var growth_value = growth_data.S12000033[key];
+            x.push(growth_value);
+            y.push(probability);
+            color.push(complexity_value);
+            text.push('Industry:&nbsp;' + '<b>' + key + '</b><br>' +
+                'Number of businesses:&nbsp;' + n_businesses + '<br>' +
+                'Probability of growth in industry in area:&nbsp;' +
+                probability + '<br>' + 'Product Complexity:&nbsp;' + complexity_value + '<br>' +
+                'Business growth 2010 - 2015:&nbsp;' + growth_value);
+            size.push(n_businesses);
         }
-    });
-
-    map.addControl(new AreaControl('areas', {
-        position: 'topright'
-    }));
-
-
-    /* Dynamic resizing */
-    $(window).on("resize", function() {
-        var h = $(window).height();
-        $("#map").height(h - (h * 0.2));
-        map.invalidateSize();
-    }).trigger("resize");
-
-    loadData();
-
-    /*--------------------------------------------------------------*/
-    /* Charts */
-
-    var chart;
-
-    function currentSelectedIndustry() {
-        return $("#indList").val();
     }
 
-    function parseData(n_businesses_data, prob, prod_complexity, growth_data) {
-        var marker_data = [];
-        var x = [];
-        var y = [];
-        var color = [];
-        var text = [];
-        var size = [];
-        for (var key in n_businesses_data.S12000033) {
-            if (key !== "growth" && key !== "LAD14NM") {
-                var n_businesses = n_businesses_data.S12000033[key];
-                var probability = prob.S12000033[key];
-                var complexity_value = prod_complexity[key].score;
-                var growth_value = growth_data.S12000033[key];
-                x.push(growth_value);
-                y.push(probability);
-                color.push(complexity_value);
-                text.push('Industry:&nbsp;' + '<b>' + key + '</b><br>' +
-                    'Number of businesses:&nbsp;' + n_businesses + '<br>' +
-                    'Probability of growth in industry in area:&nbsp;' +
-                    probability + '<br>' + 'Product Complexity:&nbsp;' + complexity_value + '<br>' +
-                    'Business growth 2010 - 2015:&nbsp;' + growth_value);
-                size.push(n_businesses);
+    marker_data = [{
+        x: x,
+        y: y,
+        mode: 'markers',
+        marker: {
+            sizemode: 'area',
+            size: size,
+            sizeref: '2em',
+            colorscale: 'YlOrRd',
+            cmin: -2.5,
+            cmax: 2.5,
+            color: color,
+            colorbar: {
+                titleside: 'right',
+                outlinecolor: 'rgba(68,68,68,0)',
+                ticks: 'none',
+                ticklen: 0,
+                tickfont: 'Arial',
+                fontsize: 4
             }
-        }
+        },
+        text: text,
+        hoverinfo: 'text'
+    }];
+    return marker_data;
+}
 
-        marker_data = [{
-            x: x,
-            y: y,
-            mode: 'markers',
-            marker: {
-                sizemode: 'area',
-                size: size,
-                sizeref: '2em',
-                colorscale: 'YlOrRd',
-                cmin: -2.5,
-                cmax: 2.5,
-                color: color,
-                colorbar: {
-                    titleside: 'right',
-                    outlinecolor: 'rgba(68,68,68,0)',
-                    ticks: 'none',
-                    ticklen: 0,
-                    tickfont: 'Arial',
-                    fontsize: 4
-                }
-            },
-            text: text,
-            hoverinfo: 'text'
-        }];
-        return marker_data;
+function analyze(error, num_businesses, prod_complexity, prob, growth) {
+    if (error) {
+        console.log(error);
     }
 
-    function analyze(error, num_businesses, prod_complexity, prob, growth) {
-        if (error) {
-            console.log(error);
-        }
+    var data = parseData(num_businesses, prob, prod_complexity, growth);
 
-        var data = parseData(num_businesses, prob, prod_complexity, growth);
+    var layout = {
+        showlegend: false,
+        autosize: true,
+        xaxis: {
+            title: 'Sector Growth 2011 - 2015',
+            autorange: true,
+            zeroline: false,
+            dtick: 0.5,
+        },
+        yaxis: {
+            title: 'Probability of Growth in Sector in Area in 2016',
+            zeroline: false,
+            dtick: 0.1
+        },
+        margin: {
+            t: 10
+        },
+        hovermode: 'closest',
+    };
+    Plotly.newPlot('chart', data, layout, {
+        showLink: false
+    });
+}
 
-        var layout = {
-            showlegend: false,
-            autosize: true,
-            xaxis: {
-                title: 'Sector Growth 2011 - 2015',
-                autorange: true,
-                zeroline: false,
-                dtick: 0.5,
-            },
-            yaxis: {
-                title: 'Probability of Growth in Sector in Area in 2016',
-                zeroline: false,
-                dtick: 0.1
-            },
-            margin: {
-                t: 10
-            },
-            hovermode: 'closest',
-        };
-        Plotly.newPlot('chart', data, layout, {
-            showLink: false
-        });
-    }
-
-    queue()
-        .defer(d3.json, "/data/business_data.json")
-        .defer(d3.json, "/data/product_complexity_2015.json")
-        .defer(d3.json, "/data/probabilities.json")
-        .defer(d3.json, "/data/business_growth_2010_2015.json")
-        .await(analyze);
+queue()
+    .defer(d3.json, "/data/business_data.json")
+    .defer(d3.json, "/data/product_complexity_2015.json")
+    .defer(d3.json, "/data/probabilities.json")
+    .defer(d3.json, "/data/business_growth_2010_2015.json")
+    .await(analyze);
